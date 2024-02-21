837915c810b60645e7d88679d6dd5a129544b3d7
Harsh <hk.develop07@gmail.com>
2024-02-21 23:01:59 +0530

    removed axios

--- Back-End/routes/internships.js
+++ Back-End/routes/internships.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const axios = require('axios');
-const { isAuthenticated, auth } = require('../middlewares/authMiddleware');
-const { InternshipDetails } = require('../Internship/InternshipDetails');
-const {applyInternship}=require("../Internship/applyInternship");
-
-
+const express = require("express");
+const { isAuthenticated, auth } = require("../middlewares/authMiddleware");
+const { InternshipDetails } = require("../Internship/InternshipDetails");
+const { applyInternship } = require("../Internship/applyInternship");
 
 const router = express.Router();
 
 // Fetch Internships
-router.post('/internships',auth, InternshipDetails);
+router.post("/internships", auth, InternshipDetails);
 
 // Apply for Internship
-router.post('/applyInternship/:internshipId',auth,applyInternship);
+router.post("/applyInternship/:internshipId", auth, applyInternship);
 
 module.exports = router;
